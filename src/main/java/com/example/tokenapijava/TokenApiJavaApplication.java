70de10e9baa019dab9f49af538066db2caea8b90
jaulinke <kevin.jaulin@utt.fr>
2026-04-01 11:29:43 +0200

    Initial Commit

---
+++ src/main/java/com/example/tokenapijava/TokenApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tokenapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenApiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenApiJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tokenapijava/TokenApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tokenapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenApiJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
