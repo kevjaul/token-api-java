8b730f6a40d5f99094437d473f2b0fc0af1c9337
kevjaul <kevin31072002@gmail.com>
2026-05-04 22:01:06 +0200

    fix(MDCLogging): Log SYSTEM for scheduled cleanup jobs

--- src/main/java/com/example/tokenapijava/config/ApiKeyCleanupJobs.java
+++ src/main/java/com/example/tokenapijava/config/ApiKeyCleanupJobs.java
@@ -11,6 +11,8 @@ import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 
+import org.slf4j.MDC;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -46,62 +48,77 @@ public class ApiKeyCleanupJobs {
     @Scheduled(cron = "0 0 3 * * *")
     @Transactional
     public void cleanupExpiredApiKeysAndApps() throws SchedulerException {
-        log.info("Core Scheduled Job: Running cleanup job.");
-        Instant now = Instant.now();
-        Instant deleteThreshold = now.minus(30, ChronoUnit.DAYS);
+        try{
+            MDC.put("apiKey","[SYSTEM]");
+            MDC.put("method","[SCHEDULER]");
 
-        // Delete expired and revoked API keys
-        List<ApiKeySchema> keysToDelete = apiKeyRepository.findAllByStatusInAndExpiresAtBefore(
-            List.of(Status.EXPIRED, Status.REVOKED), deleteThreshold
-        );
+            log.info("Core Scheduled Job: Running cleanup job.");
+            Instant now = Instant.now();
+            Instant deleteThreshold = now.minus(30, ChronoUnit.DAYS);
 
-        for (ApiKeySchema key : keysToDelete) {
-            apiKeyScopeRepository.deleteById_HashedApiKey(key.getHashedApiKey());
-            if(key.getRoleType() != Role.ADMIN){ // Do not delete admin keys
-                apiKeyRepository.delete(key);    
-                log.info("Core Scheduled Job: {} key deleted: {}...", key.getStatus(), key.getHashedApiKey().substring(0, 12));
-            }
-        }
-        
-        // Delete orphans applications
-        List<AppsSchema> apps = appsRepository.findAll();
-
-        for (AppsSchema app : apps) {
-            boolean hasScopes =
-                apiKeyScopeRepository.existsById_appId(app.getId());
-
-            if (!hasScopes) {
-                log.info("Core Scheduled Job: Application with appId={} doesn't have linked scope anymore. Deleting application...", app.getId());
-                tokenRepository.deleteAllById_AppId(app.getId());
-                log.info("Core Scheduled Job: All users deleted for application {} with appId={}", app.getAppName(), app.getId());
-                if(scheduler.checkExists(JobKey.jobKey("regen-" + app.getId()))){
-                    tokenService.deleteAppSchedule(app.getId());
-                }
-                appsRepository.delete(app);
-                log.info("Core Scheduled Job: Application {} with appId={} deleted", app.getAppName(), app.getId());
-            }
-        }
-        log.info("Core Scheduled Job: Finished cleanup job.");
-    }
-
-    @Scheduled(cron = "0 0 2 * * *")
-    @Transactional
-    public void checkAndUpdateExpiredApiKeys(){
-        log.info("Core Scheduled Job: Running expired api keys check job.");    
-        Instant now = Instant.now();
-
-        List<ApiKeySchema> keysToExpire =
-            apiKeyRepository.findAllByStatusInAndExpiresAtBefore(
-                List.of(Status.ACTIVE, Status.ROTATING), now
+            // Delete expired and revoked API keys
+            List<ApiKeySchema> keysToDelete = apiKeyRepository.findAllByStatusInAndExpiresAtBefore(
+                List.of(Status.EXPIRED, Status.REVOKED), deleteThreshold
             );
 
-        for (ApiKeySchema key : keysToExpire) {
-            if (key.getRoleType() != Role.ADMIN) {
-                Status currentStatus = key.getStatus();
-                key.setStatus(Status.EXPIRED);
-                log.info("Core Scheduled Job: {}... key expired, set is status from {} to {}", key.getHashedApiKey().substring(0, 12), currentStatus, key.getStatus());
+            for (ApiKeySchema key : keysToDelete) {
+                apiKeyScopeRepository.deleteById_HashedApiKey(key.getHashedApiKey());
+                if(key.getRoleType() != Role.ADMIN){ // Do not delete admin keys
+                    apiKeyRepository.delete(key);    
+                    log.info("Core Scheduled Job: {} key deleted: {}...", key.getStatus(), key.getHashedApiKey().substring(0, 12));
+                }
             }
+            
+            // Delete orphans applications
+            List<AppsSchema> apps = appsRepository.findAll();
+
+            for (AppsSchema app : apps) {
+                boolean hasScopes =
+                    apiKeyScopeRepository.existsById_appId(app.getId());
+
+                if (!hasScopes) {
+                    log.info("Core Scheduled Job: Application with appId={} doesn't have linked scope anymore. Deleting application...", app.getId());
+                    tokenRepository.deleteAllById_AppId(app.getId());
+                    log.info("Core Scheduled Job: All users deleted for application {} with appId={}", app.getAppName(), app.getId());
+                    if(scheduler.checkExists(JobKey.jobKey("regen-" + app.getId()))){
+                        tokenService.deleteAppSchedule(app.getId());
+                    }
+                    appsRepository.delete(app);
+                    log.info("Core Scheduled Job: Application {} with appId={} deleted", app.getAppName(), app.getId());
+                }
+            }
+            log.info("Core Scheduled Job: Finished cleanup job.");
+        } finally {
+            MDC.clear();
         }
-        log.info("Core Scheduled Job: Finished expired api keys check job.");
+        
+    }
+
+    @Scheduled(cron = "0 59 21 * * *")
+    @Transactional
+    public void checkAndUpdateExpiredApiKeys(){
+        try{
+            MDC.put("apiKey","[SYSTEM]");
+            MDC.put("method","[SCHEDULER]");
+
+            log.info("Core Scheduled Job: Running expired api keys check job.");    
+            Instant now = Instant.now();
+
+            List<ApiKeySchema> keysToExpire =
+                apiKeyRepository.findAllByStatusInAndExpiresAtBefore(
+                    List.of(Status.ACTIVE, Status.ROTATING), now
+                );
+
+            for (ApiKeySchema key : keysToExpire) {
+                if (key.getRoleType() != Role.ADMIN) {
+                    Status currentStatus = key.getStatus();
+                    key.setStatus(Status.EXPIRED);
+                    log.info("Core Scheduled Job: {}... key expired, set is status from {} to {}", key.getHashedApiKey().substring(0, 12), currentStatus, key.getStatus());
+                }
+            }
+            log.info("Core Scheduled Job: Finished expired api keys check job.");
+        } finally {
+            MDC.clear();
+        }
     }
 }
